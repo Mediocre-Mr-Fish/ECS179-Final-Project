3cf275a912ee3258f0fde7995363c522b7079425
wen-z-z <156777294+wen-z-z@users.noreply.github.com>
2025-10-19 12:02:38 -0700

    level3 and end screen

--- puzzle-platformer-game-jam/exit.gd
+++ puzzle-platformer-game-jam/exit.gd
@@ -1,6 +1,6 @@
 extends HurtBox
 
-@export var sceneFileLocation = "res://Level2.tscn"
+@export var sceneFileLocation = "res://Level1.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
