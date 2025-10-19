86072339a6216cafe741269c20333c97a6338be5
Ellison Song <ellsong@ucdavis.edu>
2025-10-19 12:41:11 -0700

    Added controls screen

--- puzzle-platformer-game-jam/changeSceneButton.gd
+++ puzzle-platformer-game-jam/changeSceneButton.gd
@@ -6,3 +6,7 @@ extends Button
 func _on_pressed() -> void:
 	print(target_scene)
 	get_tree().change_scene_to_packed(target_scene)
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
