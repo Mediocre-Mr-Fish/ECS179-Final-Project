d4c3e6b7474c18ffa7255a6bdf5a0d2f67384a36
wen-z-z <156777294+wen-z-z@users.noreply.github.com>
2025-11-13 16:49:51 -0800

    fixed instancing issues

--- puzzle-platformer-game-jam/objects/physics_box/physics_box.gd
+++ puzzle-platformer-game-jam/objects/physics_box/physics_box.gd
@@ -1,11 +1,20 @@
 class_name PhysicsBox
 extends RigidBody2D
 
+@export var size:int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mass = 0.5
-	pass # Replace with function body.
+	#resize hitbox
+	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
+	$CollisionShape2D.shape.set_size(Vector2(size, size))
+	#resize sprite
+	var scaled_width = $CollisionShape2D/Sprite2D.texture.get_width() * $CollisionShape2D/Sprite2D.scale.x 
+	var scaled_height = $CollisionShape2D/Sprite2D.texture.get_height() * $CollisionShape2D/Sprite2D.scale.y 
+	$CollisionShape2D/Sprite2D.scale = Vector2(size/scaled_width, size/scaled_height)
+	#resize boxHurtBox
+	#resize boxHitBox
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
