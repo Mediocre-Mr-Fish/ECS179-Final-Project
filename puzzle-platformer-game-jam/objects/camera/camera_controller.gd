d9002648305cedcdb068657d231affdc5c3d37df
Ellison Song <ellsong@ucdavis.edu>
2025-11-20 09:20:15 -0800

    changed camera shifting logic: detects input

--- puzzle-platformer-game-jam/objects/camera/camera_controller.gd
+++ puzzle-platformer-game-jam/objects/camera/camera_controller.gd
@@ -2,13 +2,20 @@ class_name CameraController
 extends Camera2D
 
 @export var zoom_scale:float = 1.0
+
+## the size of the pushbox
+@export var pushbox_size:Vector2 = Vector2(500,500)
+## the top left limit of the pushbox
+@export var pushbox_top_left:Vector2 = Vector2(-250, -250)
+## the bottom right limit of the pushbox
+@export var pushbox_bot_right:Vector2 = Vector2(250, 250)
+
 @export var floating_offset:Vector2 = Vector2.ZERO
 @export var shifted_offset_horizontal:Vector2 = Vector2(550, 0)
 @export var shifted_offset_up:Vector2 = Vector2(0, -300)
 @export var shifted_offset_down:Vector2 = Vector2(0, 300)
 
 @export var shift_speed:float = 2000.0
-
 @export var subject:PlayerController:
 	set(value):
 		if value:
@@ -17,32 +24,68 @@ extends Camera2D
 
 @onready var overlay:Node = $Overlay
 
-enum Overlay{
+enum Overlay {
 	NONE,
 	GREEN,
 }
 
 var current_overlay:Overlay = Overlay.NONE
+# pair of ints that describe camera shift direction. Values are only -1, 0, 1. Checked using <0, >0, ==0
+var shift: Vector2i = Vector2i.ZERO
+var pushbox_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	zoom = Vector2.ONE * zoom_scale
 	self.scale = Vector2.ONE / zoom_scale
 	
 	overlay.visible = false
+	shift = Vector2.ZERO
+	pushbox_position = Vector2.ZERO
+	
 	global_position = subject.global_position + floating_offset
 
 
+func _camera_logic() -> void:
+	var subject_offset:Vector2 = subject.global_position - global_position
+	if subject_offset.x < pushbox_top_left.x:
+		pass
+	if subject_offset.x > pushbox_bot_right.x:
+		pass
+
+func _draw() -> void:
+	var thickness:float = 3
+	var color: Color = Color.BLACK
+	
+	var pushbox_limits: Rect2 = Rect2(pushbox_top_left, pushbox_bot_right - pushbox_top_left)
+	draw_rect(pushbox_limits, color, false, thickness)
+	
+
+func _detect_shift() -> bool:
+	var pressed:bool = Input.is_action_pressed("gimick1")
+	if pressed and shift == Vector2i.ZERO:
+		@warning_ignore("narrowing_conversion")
+		shift.x = signf(Input.get_axis("left", "right"))
+		@warning_ignore("narrowing_conversion")
+		shift.y = signf(Input.get_axis("up", "down"))
+	elif pressed:
+		pass
+	else:
+		shift = Vector2i.ZERO
+	return pressed
+
+
 func _process(_delta: float) -> void:
-	var target_position:Vector2 = subject.global_position
-	if Input.is_action_pressed("gimick1"):
-		if subject.facing == subject.Facing.RIGHT:
+	var target_position:Vector2 = global_position
+	if _detect_shift():
+		target_position = subject.global_position
+		if shift.x > 0:
 			target_position += shifted_offset_horizontal
-		else:
+		elif shift.x < 0:
 			target_position -= shifted_offset_horizontal
 		
-		if subject.facing_y == subject.FacingY.UP:
+		if shift.y < 0:
 			target_position += shifted_offset_up
-		elif subject.facing_y == subject.FacingY.DOWN:
+		elif shift.y > 0:
 			target_position += shifted_offset_down
 	else:
 		target_position += floating_offset
