ac891e05c70d68c3510df272e450157dc570032f
asianfry <52115020+asianfry@users.noreply.github.com>
2025-12-08 05:50:51 -0800

    Finished level 6 albeit background is a little jank

--- puzzle-platformer-game-jam/wall_bow.gd
+++ puzzle-platformer-game-jam/wall_bow.gd
@@ -1,18 +1,41 @@
 extends Node2D
 
-@onready var wall_bow = get_tree().get_root().get_node("Wall Bow")
+#@onready var wall_bow = get_tree().get_root().get_node("Wall Bow")
+@onready var sprite = $AnimatedSprite2D
 @onready var arrow = load("res://scripts/traps/arrow.tscn")
 
 func _ready():
-	shoot()
+	fire_anim()
 	
+func fire_anim():
+	print("arrow debug1")
+	sprite.animation = "firing"
+	sprite.play("firing")
+	await sprite.animation_finished
+	_on_animation_finished()
+	
+	
+func _on_animation_finished():
+	if sprite.animation == "firing":
+		#sprite.play("firing")
+		print("arrow debug2")
+		shoot()
+		sprite.play("empty")
+		await sprite.animation_finished
+		
+		if sprite.animation == "empty":
+			print("arrow debug3")
+			sprite.play("idle")
+		
+		
 func shoot():
 	var instance = arrow.instantiate()
 	
 	instance.dir = rotation
-	instance.spawn_pos = global_position
+	instance.spawn_pos = global_position 
 	instance.spawn_rot = global_rotation
-	instance.zdex = z_index - 1
-	call_deferred("add_child", instance)
-	print("arrow spawned")
+	instance.zdex = z_index 
+	#call_deferred("add_child", instance)
+	print("arrow shot")
 	get_parent().add_child(instance)
+	
