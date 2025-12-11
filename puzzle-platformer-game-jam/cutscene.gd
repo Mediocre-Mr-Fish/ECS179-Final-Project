b43c906ca66c797713aa9fba0195a6c42983a25d
Ellison Song <ellsong@ucdavis.edu>
2025-12-10 23:09:06 -0800

    added the ablity to skip the cutscene

--- puzzle-platformer-game-jam/cutscene.gd
+++ puzzle-platformer-game-jam/cutscene.gd
@@ -4,7 +4,7 @@ const TIMER := 7.0
 
 var images := []
 var current := 0
-
+var current_timer:SceneTreeTimer
 
 func _ready():
 	images = get_children()
@@ -15,14 +15,26 @@ func _ready():
 	_show_image(0)
 
 
-func _show_image(index):
-	images[index].visible = true
-	current = index
-	await get_tree().create_timer(TIMER).timeout
-	
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		current_timer.timeout.disconnect(_on_timer_timeout)
+		_on_timer_timeout()
+
+
+func _on_timer_timeout() -> void:
 	if current + 1 < images.size():
 		images[current].visible = false
 		_show_image(current + 1)
 	else:
 		get_tree().change_scene_to_file(LevelMap.get_first_level())
-		
+		current_timer.timeout.disconnect(_on_timer_timeout)
+
+
+func _show_image(index):
+	images[index].visible = true
+	current = index
+	current_timer = get_tree().create_timer(TIMER)
+	current_timer.timeout.connect(_on_timer_timeout)
+	
+	
