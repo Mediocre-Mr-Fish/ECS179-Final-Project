c8fe9206e17f9455e91c660e24613e0639a42f7a
asianfry <52115020+asianfry@users.noreply.github.com>
2025-12-10 21:45:01 -0800

    Provided cutscene

---
+++ puzzle-platformer-game-jam/cutscene.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+const TIMER := 7.0
+
+var images := []
+var current := 0
+
+func _ready():
+	
+	images = get_children()
+	
+	for img in images:
+		img.visible = false
+	
+	_show_image(0)
+	
+func _show_image(index):
+	images[index].visible = true
+	current = index
+	await get_tree().create_timer(TIMER).timeout
+	
+	if current + 1 < images.size():
+		images[current].visible = false
+		_show_image(current + 1)
+	else:
+		images[current].visible = false
+		
